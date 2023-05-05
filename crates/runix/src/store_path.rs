033499ca58f01cc61202a980185eedf6a36de3d4
Yannik Sander <yannik@floxdev.com>
2023-05-05 16:47:06 +0200

    feat: parse more components of a store path
    * add methods to retrieve out_paths and the original path independently
    * add methods extracting prefix, package path name, and contained path independently
    * add more flexible parsing
    * document `StorePath` mathods

--- crates/runix/src/store_path.rs
+++ crates/runix/src/store_path.rs
@@ -1,12 +1,13 @@
+use std::ffi::OsStr;
 use std::fmt::Display;
-use std::path::{Path, PathBuf};
+use std::path::{Component, Path, PathBuf};
 use std::str::FromStr;
 
 use once_cell::sync::Lazy;
 use thiserror::Error;
 
 /// Respect [NIX_STORE_DIR](https://nixos.org/manual/nix/stable/command-ref/env-common.html#env-NIX_STORE_DIR)
-static STORE_PREFIX: Lazy<String> = Lazy::new(|| {
+pub static STORE_PREFIX: Lazy<String> = Lazy::new(|| {
     std::env::var("NIX_STORE_DIR")
         .ok()
         .or_else(|| option_env!("NIX_STORE_DIR").map(String::from))
@@ -14,22 +15,225 @@ static STORE_PREFIX: Lazy<String> = Lazy::new(|| {
 });
 
 #[derive(Debug, PartialEq, Eq, Clone)]
-pub struct StorePath(PathBuf);
+pub struct StorePath {
+    prefix: PathBuf,
+    name: String,
+    package_path: Option<PathBuf>,
+}
+
+impl StorePath {
+    /// nix store prefix, checked against [STORE_PREFIX]
+    ///
+    /// ```
+    /// # use std::path::Path;
+    /// # use runix::store_path::{StorePath, STORE_PREFIX};
+    ///
+    /// let path = StorePath::from_path("/nix/store/7rjqb838snvvxcmpvck1smfxhkwzqal5-python3-3.10.10")
+    ///     .unwrap();
+    /// assert_eq!(path.prefix(), Path::new(STORE_PREFIX.as_str()));
+    /// ```
+    pub fn prefix(&self) -> &Path {
+        &self.prefix
+    }
+
+    /// the package name in the store
+    ///
+    /// ```
+    /// # use std::path::Path;
+    /// # use runix::store_path::{StorePath, STORE_PREFIX};
+    ///
+    /// let path = StorePath::from_path("/nix/store/7rjqb838snvvxcmpvck1smfxhkwzqal5-python3-3.10.10")
+    ///     .unwrap();
+    /// assert_eq!(
+    ///     path.name(),
+    ///     "7rjqb838snvvxcmpvck1smfxhkwzqal5-python3-3.10.10"
+    /// );
+    /// ```
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    /// the package's path in the nix store
+    ///
+    /// drops all further components that might have been originally passed
+    ///
+    /// ```
+    /// # use std::path::Path;
+    /// # use runix::store_path::{StorePath, STORE_PREFIX};
+    ///
+    /// let out_path = "/nix/store/7rjqb838snvvxcmpvck1smfxhkwzqal5-python3-3.10.10";
+    ///
+    /// let path = StorePath::from_path(&out_path).unwrap();
+    /// assert_eq!(path.out_path(), Path::new(&out_path));
+    ///
+    /// let inside_out_path = "/nix/store/7rjqb838snvvxcmpvck1smfxhkwzqal5-python3-3.10.10/bin/python";
+    /// let path = StorePath::from_path(&inside_out_path).unwrap();
+    /// assert_eq!(path.out_path(), Path::new(&out_path)); // !! without /bin/python
+    /// ```
+    pub fn out_path(&self) -> PathBuf {
+        self.prefix.join(&self.name)
+    }
+
+    /// path of a file or directory inside the package
+    ///
+    /// [None] if not present in passed path
+    ///
+    /// ```
+    /// # use std::path::Path;
+    /// # use runix::store_path::{StorePath, STORE_PREFIX};
+    ///
+    /// let out_path = "/nix/store/7rjqb838snvvxcmpvck1smfxhkwzqal5-python3-3.10.10";
+    ///
+    /// let path = StorePath::from_path(&out_path).unwrap();
+    /// assert_eq!(path.package_path(), None);
+    ///
+    /// let inside_out_path = "/nix/store/7rjqb838snvvxcmpvck1smfxhkwzqal5-python3-3.10.10/bin/python";
+    /// let path = StorePath::from_path(&inside_out_path).unwrap();
+    /// assert_eq!(path.package_path(), Some(Path::new("bin/python")));
+    /// ```
+    pub fn package_path(&self) -> Option<&Path> {
+        self.package_path.as_deref()
+    }
+
+    /// mutable reference to path of a file or directory inside the package
+    ///
+    /// see also: [`StorePath::package_path`]
+    ///
+    /// ```ignore
+    /// /nix/store/7rjqb838snvvxcmpvck1smfxhkwzqal5-python3-3.10.10/bin/python
+    ///                                                            ^^^^^^^^^^^
+    /// ```
+    pub fn package_path_mut(&mut self) -> &mut Option<PathBuf> {
+        &mut self.package_path
+    }
+
+    /// Combine components of store path into a native path type
+    ///
+    /// If parsed from a path, should return an equivalent path
+    ///
+    /// ```
+    /// # use std::path::Path;
+    /// # use runix::store_path::{StorePath, STORE_PREFIX};
+    ///
+    /// let path = Path::new("/nix/store/7rjqb838snvvxcmpvck1smfxhkwzqal5-python3-3.10.10/bin/python");
+    /// assert_eq!(StorePath::from_path(&path).unwrap().as_path(), path);
+    /// ```
+    pub fn as_path(&self) -> PathBuf {
+        if let Some(ref path) = self.package_path {
+            self.out_path().join(path)
+        } else {
+            self.out_path()
+        }
+    }
+
+    /// Try parsing a store path from a pathbuf
+    ///
+    /// Will return an error
+    ///
+    /// * if the path is relative and does not resolve to a store path
+    /// * if the path's prefix does not equal the [STORE_PREFIX]
+    /// * if the path contains '..' components
+    /// * if the path only contains the [STORE_PREFIX]
+    /// ```
+    /// # use runix::store_path::{StorePath, StorePathError, STORE_PREFIX};
+    ///
+    /// assert!(matches!(
+    ///     StorePath::from_path("./relative"),
+    ///     Err(StorePathError::RelativePath(_, _))
+    /// ));
+    ///
+    /// assert!(matches!(
+    ///     StorePath::from_path("/var/nix/store/7rjqb838snvvxcmpvck1smfxhkwzqal5-python3-3.10.10"),
+    ///     Err(StorePathError::NotAStorePath(_))
+    /// ));
+    ///
+    /// assert!(matches!(
+    ///     StorePath::from_path(
+    ///         "/nix/store/7rjqb838snvvxcmpvck1smfxhkwzqal5-python3-3.10.10/bin/../lib"
+    ///     ),
+    ///     Err(StorePathError::RelativeComponent(_))
+    /// ));
+    ///
+    /// assert!(matches!(
+    ///     StorePath::from_path("/nix/store/"),
+    ///     Err(StorePathError::NoPackage(_))
+    /// ));
+    /// ```
+    pub fn from_path(path: impl AsRef<Path>) -> Result<Self, StorePathError> {
+        Self::try_from(path.as_ref().to_path_buf())
+    }
+
+    /// Create a store path given the out_path dir only,
+    /// substituting [STORE_PREFIX].
+    ///
+    /// Likely not the method you are looking for, try [`StorePath::from_path`]
+    pub fn new(name: impl AsRef<str>) -> Self {
+        StorePath {
+            prefix: Path::new(STORE_PREFIX.as_str()).to_path_buf(),
+            name: name.as_ref().to_string(),
+            package_path: None,
+        }
+    }
+
+    /// Create a store path from its components
+    ///
+    /// Should be used with care.
+    /// Use [`StorePath::from_path`] instead to create a validated store path
+    pub fn new_unchecked(
+        prefix: impl AsRef<Path>,
+        name: impl AsRef<str>,
+        package_path: Option<impl AsRef<Path>>,
+    ) -> Self {
+        StorePath {
+            prefix: prefix.as_ref().to_path_buf(),
+            name: name.as_ref().to_string(),
+            package_path: package_path.map(|p| p.as_ref().to_path_buf()),
+        }
+    }
+}
 
 impl TryFrom<PathBuf> for StorePath {
     type Error = StorePathError;
 
-    fn try_from(value: PathBuf) -> Result<Self, Self::Error> {
-        let path = value
+    fn try_from(mut value: PathBuf) -> Result<Self, Self::Error> {
+        // canonicalize relative path to resolve paths for `./result/` links
+        if value.is_relative() {
+            value = value
+                .canonicalize()
+                .map_err(|e| StorePathError::RelativePath(value, e))?;
+        }
+
+        let mut components = value
             .as_path()
             .strip_prefix(STORE_PREFIX.as_str())
-            .map_err(|_| StorePathError::NotAStorePath(value.clone()))?;
+            .map_err(|_| StorePathError::NotAStorePath(value.clone()))?
+            .components()
+            .peekable();
 
-        path.components()
+        if !components
+            .clone()
+            .filter(|component| {
+                component != &Component::CurDir || component != &Component::Normal(OsStr::new(""))
+            })
+            .all(|component| dbg!(matches!(component, Component::Normal(_))))
+        {
+            return Err(StorePathError::RelativeComponent(value));
+        }
+
+        let name = components
             .next()
-            .map(|store_path| Path::new(STORE_PREFIX.as_str()).join(store_path))
-            .map(StorePath)
-            .ok_or(StorePathError::NotAStorePath(value))
+            .ok_or_else(|| StorePathError::NoPackage(value.clone()))?
+            .as_os_str()
+            .to_string_lossy()
+            .into_owned();
+
+        let mut path = StorePath::new(name);
+
+        if components.peek().is_some() {
+            let _ = path.package_path_mut().insert(components.collect());
+        }
+
+        Ok(path)
     }
 }
 
@@ -43,18 +247,18 @@ impl FromStr for StorePath {
 
 impl Display for StorePath {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0.to_string_lossy())
-    }
-}
-
-impl AsRef<Path> for StorePath {
-    fn as_ref(&self) -> &Path {
-        self.0.as_ref()
+        write!(f, "{}", self.as_path().to_string_lossy())
     }
 }
 
 #[derive(Debug, Error)]
 pub enum StorePathError {
-    #[error("'{0}' is not a store path")]
+    #[error("'{0}' is a relative path and could not be canonicalized")]
+    RelativePath(PathBuf, std::io::Error),
+    #[error("'{0}' contains invalid components (e.g. '..')")]
+    RelativeComponent(PathBuf),
+    #[error("'{0}' is not a store path (not a child of {})", STORE_PREFIX.as_str())]
     NotAStorePath(PathBuf),
+    #[error("'{0}' is mising a package directory")]
+    NoPackage(PathBuf),
 }
