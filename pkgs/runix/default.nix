3ae074110a043dc6f529e1323fc5542dd5dc6928
Yannik Sander <yannik@floxdev.com>
2023-07-15 16:14:08 +0200

    build: awkward changes to the lib package shim
    We recommend devshells anyway -- needs to be cleaned up anyway

--- pkgs/runix/default.nix
+++ pkgs/runix/default.nix
@@ -1,16 +1,40 @@
-{runCommandNoCC}:
-runCommandNoCC "runix-devenv-shim" {} ''
-  cat <<EOF
-  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                          Dev env only package
+{
+  pkgs,
+  nix,
+  pkg-config,
+  boost,
+  nlohmann_json,
+  rustfmt,
+}:
+pkgs.mkShell {
+  name = "runix-devenv-shim";
+  nativeBuildInputs = with pkgs; [rustc cargo pkg-config];
+  buildInputs = with pkgs; [
+    nix
+    nlohmann_json
+    libsodium
+    boost
+    rustfmt
+    rust-analyzer
 
-    runix is a library distributed through crates.io.
-    This package is required to provide a flox development environment.
-    If you tried to build this package, instead run:
+    openssl
+  ];
 
-        $ flox develop runix
+  # provide a dummy configuration for testing
+  CONFIG_FILE = pkgs.writeText "config.toml" "";
 
-  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  EOF
-  exit 2
-''
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+}
+# runCommandCC "runix-devenv-shim" {nativeBuildInputs = [pkg-config]; buildInputs = [nix boost nlohmann_json];} ''
+#   cat <<EOF
+#   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#                           Dev env only package
+#     runix is a library distributed through crates.io.
+#     This package is required to provide a flox development environment.
+#     If you tried to build this package, instead run:
+#         $ flox develop runix
+#   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#   EOF
+#   exit 2
+# ''
+
